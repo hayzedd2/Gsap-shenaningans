a2d026c04c6973d283cbaf6a7ceae70001255a2b
hayzedd2 <azeezalhameen1@gmail.com>
2024-01-10 22:10:16 +0100

    text stagger

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
-// Draggable.create(".circle", {
-//   inertia :true,
-//   type : "x,y",
-//   bounds: ".circle-flex",
-//   ease : "bounce.out"
-// });
-
 const text = new SplitType('#p-text')
-
-// gsap.to('.char' ,{
-//     y :0,
+// gsap.from(".word" ,{
+//     y :"100%",
 //     stagger : 0.02,
-
+//     ease:"power1.out",
+//     scrollTrigger : {
+//         trigger: "split",
+//         // start : "top 80%",
+//         markers :true
+//     }
 // })
 
-// let tl = gsap.timeline({ defaults: { ease: "SlowMo.easeOut" } });
-// tl.to("span", { y: "0%", duration: 0.5, stagger: 0.1})
-
-gsap.from(".word" ,{
-    y :130,
-    stagger : 0.02,
-    ease:"back.out",
-    scrollTrigger : {
-        trigger: "split",
-        // start : "top 80%",
-        markers :true
+gsap.from(".word", {
+    y: "50%",
+    stagger: 0.025,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: "split",
+    //   start: "bottom 80%",
+      markers: true
     }
 })
+  
