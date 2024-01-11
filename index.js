7ed605cce799a382c861c805859be5548ee38107
hayzedd2 <azeezalhameen1@gmail.com>
2024-01-11 22:07:48 +0100

    new section

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
-const text = new SplitType('#p-text')
-// gsap.from(".word" ,{
-//     y :"100%",
-//     stagger : 0.02,
-//     ease:"power1.out",
-//     scrollTrigger : {
-//         trigger: "split",
-//         // start : "top 80%",
-//         markers :true
-//     }
-// })
-
-gsap.from(".word", {
-    y: "50%",
-    stagger: 0.025,
-    ease: "power1.out",
-    scrollTrigger: {
-      trigger: "split",
-    //   start: "bottom 80%",
-      markers: true
-    }
+// const text = new SplitType('#p-text')
+const text1 = new SplitType('.h6-text')
+gsap.to(".word", {
+    opacity :1,
+    stagger: 0.06,
+    ease: "bounce.out",
 })
   
