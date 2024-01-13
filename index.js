7de59149f04e610a5eb0df3c2ce2961b0b0b3963
hayzedd2 <azeezalhameen1@gmail.com>
2024-01-13 13:58:43 +0100

    svg learnings

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
-// const text = new SplitType('#p-text')
-const text1 = new SplitType('.h6-text')
+// const text1 = new SplitType('.h6-text')
 gsap.to(".word", {
     opacity :1,
     stagger: 0.06,
