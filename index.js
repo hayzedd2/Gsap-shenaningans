dab62e87375b280fe87ab773397589f631366123
hayzedd2 <azeezalhameen1@gmail.com>
2024-01-15 19:46:14 +0100

    new gsap animation

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
-const text1 = new SplitType('.h6-text')
+// const text1 = new SplitType('.h6-text')
 const text2 = new SplitType('.bg-text')
-gsap.to(text1.words, {
-    opacity :1,
-    stagger: 0.06,
-    ease: "bounce.out",
-})
+// gsap.to(text1.words, {
+//     opacity :1,
+//     stagger: 0.06,
+//     ease: "bounce.out",
+// })
   
 gsap.to(text2.lines, {
-    backgroundColor : 'red',
     duration: 0.5,
     stagger : 0.01,
 })
-  
+const divs = document.querySelectorAll(".line");
+console.log(divs.length)
+divs.forEach((div) => {
+  const createdDiv = document.createElement('div');
+  createdDiv.classList.add('createddiv')
+  div.append(createdDiv);
+  gsap.to(".createddiv", {
+    width : 0,
+    duration: 0.7,
+    stagger : 0.2,
+})
+
+});
