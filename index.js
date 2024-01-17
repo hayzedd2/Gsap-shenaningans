e386a80195ff3696ae558f3a0bd2d449ca4d4137
hayzedd2 <azeezalhameen1@gmail.com>
2024-01-17 20:07:04 +0100

    learning scrolltrigger

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
-// const text1 = new SplitType('.h6-text')
-const text2 = new SplitType('.bg-text')
-// gsap.to(text1.words, {
-//     opacity :1,
-//     stagger: 0.06,
-//     ease: "bounce.out",
-// })
-  
-gsap.to(text2.lines, {
-    duration: 0.5,
-    stagger : 0.01,
-})
-const divs = document.querySelectorAll(".line");
-console.log(divs.length)
-divs.forEach((div) => {
-  const createdDiv = document.createElement('div');
-  createdDiv.classList.add('createddiv')
-  div.append(createdDiv);
-  gsap.to(".createddiv", {
-    width : 0,
-    duration: 0.7,
-    stagger : 0.2,
-})
 
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
 });
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+const text1 = new SplitType(".h6-text");
+gsap.to(text1.words, {
+  opacity: 1,
+  stagger: 0.06,
+  ease: "bounce.out",
+});
+
+gsap.to(".square", {
+  x : 600,
+  duration : 2.5,
+  scrollTrigger : ".square",
+})
